83013fec2b698b0dc62ae986bf199694a5122db8
raisa <RAISA.22@ichat.sp.edu.sg>
2023-11-07 21:27:33 +0800

    part 2

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -5,45 +5,20 @@ from time import sleep
 led.init()
 switch.init()
 
-#Exercise 2 part b
-# while(1):
-#     if switch.read_slide_switch():
-#         led.set_output(0,1)
-#         sleep(0.1)
-#         led.set_output(0,0)
-#         sleep(0.1)
-
-#     else:
-#         led.set_output(0,0)
 
 
-#Exercise 2 part c
-# while(1):
-#     if switch.read_slide_switch():
-#         led.set_output(0,1)
-#         sleep(0.05)
-#         led.set_output(0,0)
-#         sleep(0.05)
 
-#     else:
-#         led.set_output(0,1)
-#         sleep(0.1)
-#         led.set_output(0,0)
-#         sleep(0.1)
+while(True):
+    if switch.read_slide_switch():
+        led.set_output(0,1)
+        sleep(0.05)
+        led.set_output(0,0)
+        sleep(0.05)
+
+   else:
+    led.set_output(0,1)
+    sleep(0.1)
+    led.set_output(0,0)
+    sleep(0.1)
        
 
-#Exercise 2 part d
-while(1):
-    if switch.read_slide_switch():
-        for i in range(50):
-            led.set_output(0,1)
-            sleep(0.05)
-            led.set_output(0,0)
-            sleep(0.05)
-        led.set_output(0,0)
-
-    else:
-        led.set_output(0,1)
-        sleep(0.1)
-        led.set_output(0,0)
-        sleep(0.1)
