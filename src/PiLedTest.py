6d4832bbaca2ba6e0c38d9e7e1b77cd23e4fccf0
raisa <RAISA.22@ichat.sp.edu.sg>
2023-11-08 14:49:32 +0800

    Lab4 changes final

--- src/PiLedTest.py
+++ src/PiLedTest.py
@@ -5,20 +5,31 @@ from time import sleep
 led.init()
 switch.init()
 
+def blink_led(delay):
+    # Led Blink
+    led.init()
+
+    led.set_output(0, 1)
+    time.sleep(delay)
+
+    led.set_output(0, 0)
+    time.sleep(delay)
+
+    led.set_output(0, 1)
+    time.sleep(delay)
+
+    led.set_output(0, 0)
+    time.sleep(delay)
 
 
 
-while(True):
-    if switch.read_slide_switch():
+while(switch.read_slide_switch() == 0):
+    for i in range(50):
         led.set_output(0,1)
         sleep(0.05)
         led.set_output(0,0)
         sleep(0.05)
 
-   else:
-    led.set_output(0,1)
-    sleep(0.1)
-    led.set_output(0,0)
-    sleep(0.1)
-       
+    
+
 
